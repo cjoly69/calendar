66ec9371abfc793151b3244dfc18a7f314c5c329
cjoly69 <cjoly.webdesign@gmail.com>
2016-11-17 11:04:40 +0100

    champs de saisie et affichage simple

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>calendrier</title>
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+<h1>Nouveau rendez-vous :</h1>
+    <!-- formulaire de saisie de données -->
+        <form class="" action="saisie.php" method="post">
+          <input type="texte" id="titre" name="titre" placeholder="titre">
+          <input type="date" id="debut" name="debut" placeholder="debut">
+          <input type="date" id="fin" name="fin" placeholder="fin">
+          <input type="mail" id="mail" name="mail" placeholder="votre email">
+          <input type="submit"id="btn"  name="btn" value="ajouter">
+        </form>
+
+    <?php
+    $connexion = new PDO('mysql:host=localhost;dbname=calendrier;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    ?>
+
+<div >
+
+
+<div class="liste">
+<?php
+//requete pour la table saisie
+    $requete = "SELECT * FROM saisie";
+	  $resultats = $connexion->query($requete);
+    echo "<h2>Calendrier :</h2>";
+
+	while( $calendar = $resultats->fetch() ){
+		echo "<div>Titre : " . $calendar["titre"] . "</div>";
+    echo "<div> Du " . $calendar["debut"] . " au " . $calendar["fin"] . " .</div>";
+    echo "<div>Mail de l'auteur : " . $calendar["mail"] . "</div>";
+    }
+	 $resultats->closeCursor();
+  ?>
+</div>
+
+</div>
+</br>
+  </body>
+</html>
---
+++ saisie.php
@@ -0,0 +1,22 @@
+<?php
+header('Location: index.php');
+$titre = $_POST['titre'];
+$debut = $_POST['debut'];
+$fin = $_POST['fin'];
+$mail = $_POST['mail'];
+$connexion = new PDO('mysql:host=localhost; dbname=calendrier;charset=utf8','root','');
+//
+if ((isset($_POST['titre']) && $_POST['titre']!="") && (isset($_POST['debut']) && $_POST['debut']!="") && (isset($_POST['fin']) && $_POST['fin']!="") && (isset($_POST['mail']) && $_POST['mail']!=""))
+{
+  //requète
+$insertion = "INSERT INTO saisie (titre, debut, fin, mail) VALUES ( :titre, :debut, :fin, :mail)";
+$rq = $connexion->prepare($insertion);// insertion
+
+$rq->bindParam(":titre",$titre, PDO::PARAM_STR);
+$rq->bindParam(":debut",$debut, PDO::PARAM_STR);
+$rq->bindParam(":fin",$fin, PDO::PARAM_STR);
+$rq->bindParam(":mail",$mail, PDO::PARAM_STR);
+$rq->execute();
+}
+
+?>
